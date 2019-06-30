e31c448bbbb2b5f1123309a67315e08217a02d0c
Jennifer Ross <ross.m.jennifer@gmail.com>
2019-06-30 11:18:06 -0400

    restructured functions to fix bugs and added win/loss sounds;

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -9,68 +9,101 @@ $(document).ready(function() {
   let userSales = 0;
   $("#total-score").text("Your sales: $" + userSales);
 
-  let clickSound = new Audio("assets/sound/cha-ching.wav");
+  let clickSound = new Audio("assets/sound/coin-drop.mp3");
+  let winSound = new Audio("assets/sound/win-sound.wav");
+  let lossSound = new Audio("assets/sound/acid-bubble.wav");
 
   let crystalOne = Math.floor(Math.random() * 12) + 1;
   let crystalTwo = Math.floor(Math.random() * 12) + 1;
   let crystalThree = Math.floor(Math.random() * 12) + 1;
   let crystalFour = Math.floor(Math.random() * 12) + 1;
-  
-  function genCrystalValues() {
-    $("#crystal-one").attr("data-crystalvalue", crystalOne);
-    $("#crystal-two").attr("data-crystalvalue", crystalTwo);
-    $("#crystal-three").attr("data-crystalvalue", crystalThree);
-    $("#crystal-four").attr("data-crystalvalue", crystalFour);
-  }
+
+  let winningAmount = Math.floor(19 + Math.random() * (120 + 1 - 19));
+  $("#target-number").text("$" + winningAmount);
+
 
   function initialize() {
 
-    let userSales = 0;
+    userSales = 0;
     $("#total-score").text("Your sales: $" + userSales);
   
-    let winningAmount = Math.floor(19 + Math.random() * (120 + 1 - 19));
+    winningAmount = Math.floor(19 + Math.random() * (120 + 1 - 19));
     $("#target-number").text("$" + winningAmount);
-    console.log(winningAmount);
+
+    crystalOne = Math.floor(Math.random() * 12) + 1;
+    crystalTwo = Math.floor(Math.random() * 12) + 1;
+    crystalThree = Math.floor(Math.random() * 12) + 1;
+    crystalFour = Math.floor(Math.random() * 12) + 1;
  
-    $(".crystal-images").on("click", function() {
+  } 
 
-      $("#directions").text("");
+  function didYouWin() {
+    if (userSales === winningAmount) {
+      $("#directions").text("Walt will be pleased.");
+      wins++;
+      $("#wins").text("Wins: " + wins);
+      winSound.play();
+      initialize();
+    }
 
-      clickSound.play();
-
-      let crystalValue = ($(this).attr("data-crystalvalue"));
-      crystalValue = parseInt(crystalValue);
-
-      userSales += crystalValue;
-      $("#total-score").text("Your sales: $" + userSales);
-      console.log(userSales);
-
-      if (userSales === winningAmount) {
-        $("#directions").text("Walt will be pleased.");
-        wins++;
-        $("#wins").text("Wins: " + wins);
-        $("#crystal-one").empty();
-        $("#crystal-two").empty();
-        $("#crystal-three").empty();
-        $("#crystal-four").empty();
-        genCrystalValues();
-        initialize();
-      }
-
-      else if (userSales > winningAmount) {
-        $("#directions").text("This won't be pretty.");
-        losses++;
-        $("#losses").text("Losses: " + losses);
-        $("#crystal-one").empty();
-        $("#crystal-two").empty();
-        $("#crystal-three").empty();
-        $("#crystal-four").empty();
-        genCrystalValues();
-        initialize();
-      }
-
-    });
+    else if (userSales > winningAmount) {
+      $("#directions").text("This won't be pretty.");
+      losses++;
+      $("#losses").text("Losses: " + losses);
+      lossSound.play();
+      initialize();
+    }
   }
-  genCrystalValues();
-  initialize();
+
+  $("#crystal-one").on("click", function() {
+
+    $("#directions").text("");
+    
+    clickSound.play();
+
+    userSales += crystalOne;
+    $("#total-score").text("Your sales: $" + userSales);
+    console.log(userSales);
+
+    didYouWin(); 
+  });
+
+  $("#crystal-two").on("click", function() {
+
+    $("#directions").text("");
+    
+    clickSound.play();
+
+    userSales += crystalTwo;
+    $("#total-score").text("Your sales: $" + userSales);
+    console.log(userSales);
+
+    didYouWin(); 
+  });
+
+  $("#crystal-three").on("click", function() {
+
+    $("#directions").text("");
+    
+    clickSound.play();
+
+    userSales += crystalThree;
+    $("#total-score").text("Your sales: $" + userSales);
+    console.log(userSales);
+
+    didYouWin(); 
+  });
+
+  $("#crystal-four").on("click", function() {
+
+    $("#directions").text("");
+    
+    clickSound.play();
+
+    userSales += crystalFour;
+    $("#total-score").text("Your sales: $" + userSales);
+    console.log(userSales);
+
+    didYouWin(); 
+  });
 });
