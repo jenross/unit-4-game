a899b4250590e8211c94a6d572f39bfcbda0d1a1
Jennifer Ross <ross.m.jennifer@gmail.com>
2019-07-01 20:52:04 -0400

    did not need initial line and wrap

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,109 +1,107 @@
-$(document).ready(function() {
+let wins= 0;
+$("#wins").text("Wins: " + wins);
 
-  let wins= 0;
-  $("#wins").text("Wins: " + wins);
+let losses= 0;
+$("#losses").text("Losses: " + losses);
 
-  let losses= 0;
-  $("#losses").text("Losses: " + losses);
+let userSales = 0;
+$("#total-score").text("Your sales: $" + userSales);
 
-  let userSales = 0;
+let clickSound = new Audio("assets/sound/coin-drop.mp3");
+let winSound = new Audio("assets/sound/win-sound.wav");
+let lossSound = new Audio("assets/sound/acid-bubble.wav");
+
+let crystalOne = Math.floor(Math.random() * 12) + 1;
+let crystalTwo = Math.floor(Math.random() * 12) + 1;
+let crystalThree = Math.floor(Math.random() * 12) + 1;
+let crystalFour = Math.floor(Math.random() * 12) + 1;
+
+let winningAmount = Math.floor(19 + Math.random() * (120 + 1 - 19));
+$("#target-number").text("$" + winningAmount);
+
+
+function initialize() {
+
+  userSales = 0;
   $("#total-score").text("Your sales: $" + userSales);
-
-  let clickSound = new Audio("assets/sound/coin-drop.mp3");
-  let winSound = new Audio("assets/sound/win-sound.wav");
-  let lossSound = new Audio("assets/sound/acid-bubble.wav");
-
-  let crystalOne = Math.floor(Math.random() * 12) + 1;
-  let crystalTwo = Math.floor(Math.random() * 12) + 1;
-  let crystalThree = Math.floor(Math.random() * 12) + 1;
-  let crystalFour = Math.floor(Math.random() * 12) + 1;
-
-  let winningAmount = Math.floor(19 + Math.random() * (120 + 1 - 19));
+  
+  winningAmount = Math.floor(19 + Math.random() * (120 + 1 - 19));
   $("#target-number").text("$" + winningAmount);
 
-
-  function initialize() {
-
-    userSales = 0;
-    $("#total-score").text("Your sales: $" + userSales);
-  
-    winningAmount = Math.floor(19 + Math.random() * (120 + 1 - 19));
-    $("#target-number").text("$" + winningAmount);
-
-    crystalOne = Math.floor(Math.random() * 12) + 1;
-    crystalTwo = Math.floor(Math.random() * 12) + 1;
-    crystalThree = Math.floor(Math.random() * 12) + 1;
-    crystalFour = Math.floor(Math.random() * 12) + 1;
+  crystalOne = Math.floor(Math.random() * 12) + 1;
+  crystalTwo = Math.floor(Math.random() * 12) + 1;
+  crystalThree = Math.floor(Math.random() * 12) + 1;
+  crystalFour = Math.floor(Math.random() * 12) + 1;
  
-  } 
+} 
 
-  function didYouWin() {
-    if (userSales === winningAmount) {
-      $("#directions").text("Walt will be pleased.");
-      wins++;
-      $("#wins").text("Wins: " + wins);
-      winSound.play();
-      initialize();
-    }
-
-    else if (userSales > winningAmount) {
-      $("#directions").text("This won't be pretty.");
-      losses++;
-      $("#losses").text("Losses: " + losses);
-      lossSound.play();
-      initialize();
-    }
+function didYouWin() {
+  if (userSales === winningAmount) {
+    $("#directions").text("Walt will be pleased.");
+    wins++;
+    $("#wins").text("Wins: " + wins);
+    winSound.play();
+    initialize();
   }
 
-  $("#crystal-one").on("click", function() {
+  else if (userSales > winningAmount) {
+    $("#directions").text("This won't be pretty.");
+    losses++;
+    $("#losses").text("Losses: " + losses);
+    lossSound.play();
+    initialize();
+  }
+}
 
-    $("#directions").text("");
+$("#crystal-one").on("click", function() {
+
+  $("#directions").text("");
     
-    clickSound.play();
+  clickSound.play();
 
-    userSales += crystalOne;
-    $("#total-score").text("Your sales: $" + userSales);
-    console.log(userSales);
+  userSales += crystalOne;
+  $("#total-score").text("Your sales: $" + userSales);
+  console.log(userSales);
 
-    didYouWin(); 
-  });
-
-  $("#crystal-two").on("click", function() {
-
-    $("#directions").text("");
-    
-    clickSound.play();
-
-    userSales += crystalTwo;
-    $("#total-score").text("Your sales: $" + userSales);
-    console.log(userSales);
-
-    didYouWin(); 
-  });
-
-  $("#crystal-three").on("click", function() {
-
-    $("#directions").text("");
-    
-    clickSound.play();
-
-    userSales += crystalThree;
-    $("#total-score").text("Your sales: $" + userSales);
-    console.log(userSales);
-
-    didYouWin(); 
-  });
-
-  $("#crystal-four").on("click", function() {
-
-    $("#directions").text("");
-    
-    clickSound.play();
-
-    userSales += crystalFour;
-    $("#total-score").text("Your sales: $" + userSales);
-    console.log(userSales);
-
-    didYouWin(); 
-  });
+  didYouWin(); 
 });
+
+$("#crystal-two").on("click", function() {
+
+  $("#directions").text("");
+    
+  clickSound.play();
+
+  userSales += crystalTwo;
+  $("#total-score").text("Your sales: $" + userSales);
+  console.log(userSales);
+
+  didYouWin(); 
+});
+
+$("#crystal-three").on("click", function() {
+
+  $("#directions").text("");
+    
+  clickSound.play();
+
+  userSales += crystalThree;
+  $("#total-score").text("Your sales: $" + userSales);
+  console.log(userSales);
+
+  didYouWin(); 
+});
+
+$("#crystal-four").on("click", function() {
+
+  $("#directions").text("");
+    
+  clickSound.play();
+
+  userSales += crystalFour;
+  $("#total-score").text("Your sales: $" + userSales);
+  console.log(userSales);
+
+  didYouWin(); 
+});
+
